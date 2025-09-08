c5b83e38281c2db8252a6e22f97b7db622cd1b58
jyotiswaroop20 <jyotiswaroop.niit1@gmail.com>
2025-09-08 23:45:08 +0530

    my python new code

---
+++ python_programming_journey/fifth.py
@@ -0,0 +1,55 @@
+# mx = 120
+# b = 128
+# y = mx + b
+# print(y)
+ 
+# x = 5
+# y = 5**2
+# print(y)
+
+# x = 34
+# print(x)
+# x = 50
+# print(x)
+
+# a = 5
+# b = 7
+# c = (a + b)**2
+# print(c)
+
+# x = 34 # Variable x is assigned value 34
+# print(x)
+# x = 90 # Variable x is assigned value 90
+# print(x)
+
+# y = x + 40 # y is assigned x + 40 which is 90 + 40, or 130
+# print(x) # Still 90! Nothing changed here
+# print(y) # Prints "130"
+
+# m1 = 50
+# m1 = 79
+# m1 = 89
+# y = m1
+# print(y)
+
+# print(2 + 5)
+# print(10 -4)
+# print(7 * 2)
+# print(5 / 2)
+# print(5 // 2)
+# print(2**3)
+
+y = 8 + 4 / 2 # result = 10 according to bodmos rule
+print(y)
+
+y = (8 + 4) / 2 # result = 6 according to bodmos rule
+print(y)
+
+x = 20
+x += 10 # that is similar to x = x + 10
+print(x)
+
+z = 50
+z /= 10 # that is similar to z = z / 10
+print(z)
+
---
+++ python_programming_journey/seven.py
@@ -0,0 +1,16 @@
+# x = 10
+# y = x
+# print(y)
+
+# x = 1000
+# y = x
+# y = 2000**3
+# print(y)
+
+import math
+
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+
+y = a**2 + b**2
+print("Result:", y)
